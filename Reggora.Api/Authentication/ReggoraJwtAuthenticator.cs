5d05fb18420805b33d104ceac33547da1629e0a2
JackNoordhuis <jackjnoordhuis@gmail.com>
2019-07-31 00:17:32 +1000

    Always add auth and integration tokens to requests

--- Reggora.Api/Authentication/ReggoraJwtAuthenticator.cs
+++ Reggora.Api/Authentication/ReggoraJwtAuthenticator.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Linq;
 using RestSharp;
 using RestSharp.Authenticators;
 
@@ -18,13 +16,8 @@ namespace Reggora.Api.Authentication
 
         public void Authenticate(IRestClient client, IRestRequest request)
         {
-            // only add the Authorization parameter if it hasn't been added by a previous Execute
-            if (!request.Parameters.Any(p => p.Type.Equals(ParameterType.HttpHeader) &&
-                                             p.Name.Equals("Authorization", StringComparison.OrdinalIgnoreCase)))
-            {
-                request.AddParameter("Authorization", $"Bearer {_authToken}", ParameterType.HttpHeader);
-                request.AddParameter("integration", _integrationToken);
-            }
+            request.AddParameter("Authorization", $"Bearer {_authToken}", ParameterType.HttpHeader);
+            request.AddParameter("integration", _integrationToken, ParameterType.HttpHeader);
         }
     }
 }
