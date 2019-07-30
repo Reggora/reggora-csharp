baf042d0db3799d6a7bbfe5cd337e53d4e829e85
JackNoordhuis <jackjnoordhuis@gmail.com>
2019-07-30 15:03:11 +1000

    Simple example project to show how to use the API

---
+++ Reggora.Api.Example/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Reflection;
+
+namespace Reggora.Api.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lender = Reggora.Lender(Config.GetProperty("lender.email", ""),
+                Config.GetProperty("lender.password", ""), Config.GetProperty("lender.token", ""));
+            
+//            var vendor = Reggora.Vendor(Config.GetProperty("vendor.email", ""),
+//                Config.GetProperty("vendor.password", ""), Config.GetProperty("vendor.token", ""));
+        }
+    }
+
+    public class Config
+    {
+        public static string ConfigFileName = "example.conf";
+        private static IReadOnlyDictionary<string, string> KeyValues { get; set; }
+
+        static Config()
+        {
+            try
+            {
+                string username = Environment.UserName;
+
+                string fileContents = string.Empty;
+                string path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                if (path != null)
+                {
+                    var configFilePath = Path.Combine(path, $"example.{username}.conf");
+                    if (File.Exists(configFilePath))
+                    {
+                        fileContents = File.ReadAllText(configFilePath);
+                    }
+                    else
+                    {
+                        configFilePath = Path.Combine(path, ConfigFileName);
+
+                        if (File.Exists(configFilePath))
+                        {
+                            fileContents = File.ReadAllText(configFilePath);
+                        }
+                    }
+                }
+
+                LoadValues(fileContents);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error configuring parser");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void LoadValues(string data)
+        {
+            Dictionary<string, string> newDictionairy = new Dictionary<string, string>();
+            foreach (
+                string rawLine in data.Split(new[] {"\r\n", "\n", Environment.NewLine},
+                    StringSplitOptions.RemoveEmptyEntries))
+            {
+                string line = rawLine.Trim();
+                if (line.StartsWith("#") || !line.Contains("=")) continue; //It's a comment or not a key value pair.
+
+                string[] splitLine = line.Split('=', 2);
+
+                string key = splitLine[0].ToLower();
+                string value = splitLine[1];
+                if (!newDictionairy.ContainsKey(key))
+                {
+                    newDictionairy.Add(key, value);
+                }
+            }
+
+            KeyValues = new ReadOnlyDictionary<string, string>(newDictionairy);
+        }
+
+        public static Boolean GetProperty(string property, bool defaultValue)
+        {
+            try
+            {
+                string d = ReadString(property);
+                if (d == null) return defaultValue;
+
+                return Convert.ToBoolean(d);
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+
+        public static int GetProperty(string property, int defaultValue)
+        {
+            try
+            {
+                var value = ReadString(property);
+                if (value == null) return defaultValue;
+
+                return Convert.ToInt32(value);
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+
+        public static string GetProperty(string property, string defaultValue)
+        {
+            return ReadString(property) ?? defaultValue;
+        }
+
+        private static string ReadString(string property)
+        {
+            property = property.ToLower();
+            if (!KeyValues.ContainsKey(property)) return null;
+            return KeyValues[property];
+        }
+    }
+}
