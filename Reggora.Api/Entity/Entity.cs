d5c52f33c4722d33b65e135719ad05f085d3ff86
JackNoordhuis <jackjnoordhuis@gmail.com>
2019-08-01 23:29:40 +1000

    Introduce entity relationship fields

--- Reggora.Api/Entity/Entity.cs
+++ Reggora.Api/Entity/Entity.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Linq;
+using Reggora.Api.Storage;
 
 namespace Reggora.Api.Entity
 {
@@ -13,9 +15,33 @@ namespace Reggora.Api.Entity
             BuildField(ref Id, nameof(Id));
         }
 
+        public bool Dirty()
+        {
+            return DirtyFields.Any();
+        }
+
+        public void Clean()
+        {
+            DirtyFields.Clear();
+        }
+
         protected void BuildField<T>(ref EntityField<T> field, string name)
         {
             field = new EntityField<T>(name, propertyName => DirtyFields.Add(propertyName));
+        }
+
+        protected void BuildRelationship<E>(ref EntityRelationship<E> relationship)
+            where E : Entity, new()
+        {
+            relationship = new EntityRelationship<E>(new E());
+        }
+
+        protected void BuildManyRelationship<E, C, S>(ref EntityManyRelationship<E, C, S> relationship)
+            where E : Entity
+            where C : ApiClient<C>
+            where S : Storage<E, C>, new()
+        {
+            relationship = new EntityManyRelationship<E, C, S>();
         }
     }
 }
