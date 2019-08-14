d89fd9659ccbd8d401ff16bbec4a6cf0bbc2d692
JackNoordhuis <jackjnoordhuis@gmail.com>
2019-08-14 22:33:29 +1000

    Cleanup api client class

--- Reggora.Api/ApiClient.cs
+++ Reggora.Api/ApiClient.cs
@@ -15,23 +15,8 @@ namespace Reggora.Api
         {
             IntegrationToken = integrationToken;
             Client = new RestClient(Reggora.BaseUrl);
-            Client.Proxy = new WebProxy("127.0.0.1", 8888);
         }
 
         public abstract T Authenticate(string email, string password);
-        
-        public T Execute<T>(RestRequest request) where T : new()
-        {
-            request.JsonSerializer = new NewtonsoftJsonSerializer(new JsonSerializer{NullValueHandling = NullValueHandling.Ignore});
-            
-            var response = Client.Execute<T>(request);
-
-            if (response.ErrorException != null)
-            {
-                throw Reggora.RaiseRequestErrorToException(response.StatusCode, response.ErrorException);
-            }
-
-            return response.Data;
-        }
     }
 }
