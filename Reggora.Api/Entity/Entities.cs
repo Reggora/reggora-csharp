299bc71cf743f4f186a470a8b4991bfe327ecc9c
JackNoordhuis <jackjnoordhuis@gmail.com>
2019-08-15 02:13:14 +1000

    Fix nullable entity fields

--- Reggora.Api/Entity/Entities.cs
+++ Reggora.Api/Entity/Entities.cs
@@ -10,11 +10,11 @@ namespace Reggora.Api.Entity
     {
 
         public string Id { get => _id.Value; set => _id.Value = value; }
-        public int Number { get => _number.Value; set => _number.Value = value; }
+        public int? Number { get => _number.Value; set => _number.Value = value; }
         public string Type { get => _type.Value; set => _type.Value = value; }
-        public DateTime Due { get => _due.Value; set => _due.Value = value; }
-        public DateTime Created { get => _created.Value; set => _created.Value = value; }
-        public DateTime Updated { get => _updated.Value; set => _updated.Value = value; }
+        public DateTime? Due { get => _due.Value; set => _due.Value = value; }
+        public DateTime? Created { get => _created.Value; set => _created.Value = value; }
+        public DateTime? Updated { get => _updated.Value; set => _updated.Value = value; }
         public string PropertyAddress { get => _propertyAddress.Value; set => _propertyAddress.Value = value; }
         public string PropertyCity { get => _propertyCity.Value; set => _propertyCity.Value = value; }
         public string PropertyState { get => _propertyState.Value; set => _propertyState.Value = value; }
@@ -22,11 +22,11 @@ namespace Reggora.Api.Entity
         public string CaseNumber { get => _caseNumber.Value; set => _caseNumber.Value = value; }
 
         private readonly EntityField<string> _id;
-        private readonly EntityField<int> _number;
+        private readonly EntityField<int?> _number;
         private readonly EntityField<string> _type;
-        private readonly EntityField<DateTime> _due;
-        private readonly EntityField<DateTime> _created;
-        private readonly EntityField<DateTime> _updated;
+        private readonly EntityField<DateTime?> _due;
+        private readonly EntityField<DateTime?> _created;
+        private readonly EntityField<DateTime?> _updated;
         private readonly EntityField<string> _propertyAddress;
         private readonly EntityField<string> _propertyCity;
         private readonly EntityField<string> _propertyState;
@@ -64,19 +64,19 @@ namespace Reggora.Api.Entity
 
         public string Id { get => _id.Value; set => _id.Value = value; }
         public string Status { get => _status.Value; set => _status.Value = value; }
-        public PriorityType Priority { get => _priority.Value; set => _priority.Value = value; }
-        public DateTime Due { get => _due.Value; set => _due.Value = value; }
-        public DateTime InspectedAt { get => _inspectedAt.Value; set => _inspectedAt.Value = value; }
-        public DateTime Updated { get => _updated.Value; set => _updated.Value = value; }
+        public PriorityType? Priority { get => _priority.Value; set => _priority.Value = value; }
+        public DateTime? Due { get => _due.Value; set => _due.Value = value; }
+        public DateTime? InspectedAt { get => _inspectedAt.Value; set => _inspectedAt.Value = value; }
+        public DateTime? Updated { get => _updated.Value; set => _updated.Value = value; }
         public AllocationMode Allocation { get => _allocation.Value; set => _allocation.Value = value; }
         public bool Inspected { get => _inspected.Value; set => _inspected.Value = value; }
 
         private readonly EntityField<string> _id;
         private readonly EntityField<string> _status;
-        private readonly EntityField<PriorityType> _priority;
-        private readonly EntityField<DateTime> _due;
-        private readonly EntityField<DateTime> _inspectedAt;
-        private readonly EntityField<DateTime> _updated;
+        private readonly EntityField<PriorityType?> _priority;
+        private readonly EntityField<DateTime?> _due;
+        private readonly EntityField<DateTime?> _inspectedAt;
+        private readonly EntityField<DateTime?> _updated;
         private readonly EntityField<AllocationMode> _allocation;
         private readonly EntityField<bool> _inspected;
 
