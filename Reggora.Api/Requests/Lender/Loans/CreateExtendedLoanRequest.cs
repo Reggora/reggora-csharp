37da7d66ef38c860d0dd3a0e752757d01208346f
Abdullah Mamun <amamun@reggora.com>
2020-03-26 15:11:22 -0400

    Added loan's extedded methods

---
+++ Reggora.Api/Requests/Lender/Loans/CreateExtendedLoanRequest.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reggora.Api.Requests.Lender.Loans
+{
+    public class CreateExtendedLoanRequest : ReggoraRequest
+    {
+        public CreateExtendedLoanRequest(Dictionary<string, object> loanParams) : base("lender/extended_loan", RestSharp.Method.POST)
+        {
+            AddJsonBody(new Request 
+            { 
+                Loan = loanParams
+            });
+        }
+
+        public class Request
+        {
+            [JsonProperty("loan")]
+            public Dictionary<string, object> Loan { get; set; }
+        }
+    }
+}
---
+++ Reggora.Api/Requests/Lender/Loans/GetLoanSchemaRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reggora.Api.Requests.Lender.Loans
+{
+    public class GetLoanSchemaRequest: ReggoraRequest
+    {
+        public GetLoanSchemaRequest() : base("lender/extended_loan", RestSharp.Method.GET) 
+        { 
+            
+        }
+    }
+}
